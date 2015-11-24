ca46c83e4d70e11256193fecf69ae17e25a2adca
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2015-11-23 17:18:51 -0800

    Add the SetupTools code for a smooth installation

---
+++ setup.py
@@ -0,0 +1,18 @@
+﻿from setuptools import setup, find_packages
+
+DESCRIPTION = """
+Ytranslate (Why translate) is a simple tool to manage translations
+through creating and updating losely-defined YAML catalogs.
+""".strip()
+
+setup(
+    name = "ytranslate",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = ['pyyaml>=3'],
+    description = DESCRIPTION,
+    author = 'Vincent Le Goff',
+    author_email = 'vincent.legoff.srs@gmail.com',
+    url = 'https://github.com/vlegoff/ytranslate',
+    keywords = ['translation', 'translate', 'yaml', 'yml'],
+)
